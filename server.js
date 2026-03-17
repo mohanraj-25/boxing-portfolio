69a9e96e8c1b583feada0b8eb5ff762be6ef9deb
Mohanraj-25 <mohanrajsssnc@gmail.com>
2026-03-17 07:47:58 +0530

    Added render start script

--- server.js
+++ server.js
@@ -79,7 +79,7 @@ app.post("/register", (req, res) => {
 // START SERVER
 // ===============================
 
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => {
   console.log(`🥊 Boxing Server running on http://localhost:${PORT}`);
